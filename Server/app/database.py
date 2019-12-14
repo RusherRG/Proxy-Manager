5fd675dafb29bbb5fc795afdda45055b54c695f2
RusherRG <rushang101@gmail.com>
2019-12-14 13:42:10 +0530

    Database connection done

--- Server/app/database.py
+++ Server/app/database.py
@@ -1,15 +1,15 @@
 from pymongo import MongoClient
 import os
 
-mongo_uri = 'mongodb://{0}:{1}'.format(
-    "os.environ['mongo_user']", "os.environ['mongo_pass']")
+MONGODB_HOST = os.environ['MONGODB_HOST']
+MONGODB_PORT = int(os.environ['MONGODB_PORT'])
 
 
 def connect():
     """
     Create a connection to database
     """
-    client = MongoClient(mongo_uri)
+    client = MongoClient(MONGODB_HOST, MONGODB_PORT)
     db = client.distributed_requests
     return db
 
@@ -22,5 +22,22 @@ def add_request(request_json):
     try:
         db.insert(request_json)
         return True
-    except:
+    except Exception as exc:
+        print(exc)
         return False
+
+
+def get_requests():
+    """
+    Get all requests from the database
+    """
+    db = connect().requests
+    try:
+        requests_list = list(db.find())
+        for req in requests_list:
+            del req['_id']
+        # print(requests_list)
+        return requests_list
+    except Exception as err:
+        print(err)
+        return []

181c4653311f3a174e195de56db89f04ba21c7de
RusherRG <rushang101@gmail.com>
2020-04-30 15:43:54 +0530

    Update

--- Server/app.py
+++ Server/app.py
@@ -21,7 +21,7 @@ user_agent_rotator = UserAgent(
 app = Flask(__name__)
 app.config.from_object('config')
 
-es = Elasticsearch(hosts="http://elastic:1234567Ee@34.94.244.167:9200/")
+es = Elasticsearch(hosts="http://elastic:1234567Ee@elasticsearch:9200/")
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -94,7 +94,7 @@ def get_logger():
     logger = logging.getLogger("Server")
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logstash.TCPLogstashHandler(
-        '34.94.244.167', 6000, version=1))
+        '34.94.244.167', 7000, version=1))
 
     return logger
 
